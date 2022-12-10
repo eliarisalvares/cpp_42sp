5ba1041ed0e54f7f90b93f465a8109ecff801b66
Eliaris Alvares <likah.alvares@rocketmail.com>
2022-12-10 14:19:11 -0300

    add: megaphone.cpp

---
+++ module_00/ex00/megaphone.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/10 14:12:49 by elraira-          #+#    #+#             */
+/*   Updated: 2022/12/10 14:18:14 by elraira-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int i = 1;
+	if (argc ==1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	while (i < argc)
+	{
+		while (*argv[i])
+		{
+			*argv[i] = toupper(*argv[i]);
+			std::cout << *argv[i];
+			argv[i]++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+}
