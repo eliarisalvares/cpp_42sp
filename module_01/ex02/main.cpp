ca25151799c6cd1e420dcbbb5b82c73b88137bc9
Eliaris Alvares <likah.alvares@rocketmail.com>
2023-02-26 17:26:23 -0300

    feat: brain

---
+++ module_01/ex02/main.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/26 17:02:48 by elraira-          #+#    #+#             */
+/*   Updated: 2023/02/26 17:25:09 by elraira-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream> /* cout, endl */
+#include <string> /* string */
+
+/**
+ * @brief This exercise is about references and pointers. A stringPTR and a
+ * stringREF are created.
+ * Both pointers and references are are used to have one variable point to
+ * another variable. A pointer is a variable that holds the address of another
+ * variable. A reference is a variable that is an alias for another variable.
+ * The difference between a pointer and a reference is that a pointer can be
+ * reassigned to point to another variable, while a reference cannot be
+ * reassigned to refer to another variable. A reference must be initialized
+ * when it is created. A pointer can be initialized at any time. A reference
+ * cannot be NULL while a pointer can be NULL.
+ * A pointer also has its own address, while a reference does not. A reference
+ * shares the same address as the variable it refers to but still takes up
+ * storage space.
+ *
+ */
+int	main(void)
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	std::cout << "Addresses:" << std::endl;
+	std::cout << "The address of strPTR is: " << &stringPTR << std::endl;
+	std::cout << "The address of str is: " << &str << std::endl;
+	std::cout << "The address of stringREF is: " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "Addresses held:" << std::endl;
+	std::cout << "The address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The address held by stringREF: " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "Values:" << std::endl;
+	std::cout << "The value of stringPTR is: " << stringPTR << std::endl;
+	std::cout << "The value of stringREF is: " << stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "Values pointed:" << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value referenced by stringREF: " << stringREF << std::endl;
+	return (0);
+}
