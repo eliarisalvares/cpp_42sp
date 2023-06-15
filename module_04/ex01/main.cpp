88bf0d6ddbe9310790afc1b24bd945716bc6631d
Eliaris Alvares <likah.alvares@rocketmail.com>
2023-06-14 23:04:37 -0300

    feat: ex01

---
+++ module_04/ex01/main.cpp
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/14 22:20:59 by elraira-          #+#    #+#             */
+/*   Updated: 2023/06/14 23:03:17 by elraira-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "Animal.hpp"
+
+int main(void)
+{
+    {
+        std::cout << BLUE << "Creating a Dog and a Cat on the heap:"
+            << RESET << std::endl;
+        const Animal* dog = new Dog();
+        const Animal* cat = new Cat();
+
+        std::cout << std::endl;
+        std::cout << BLUE << "Creating a Dog and a Cat on the stack:"
+            << RESET << std::endl;
+        Dog dog2;
+        Cat cat2;
+
+        std::cout << std::endl;
+        std::cout << BLUE << "Deleting the animals:" << RESET << std::endl;
+        delete dog;
+        delete cat;
+
+        std::cout << std::endl;
+        std::cout << BLUE << "Exiting the scope:" << RESET << std::endl;
+    }
+    {
+        std::cout << std::endl;
+        std::cout << BLUE_BACKGROUND << "Creating an array of 30 animals:"
+            << RESET << std::endl;
+        const int size = 30;
+        const Animal* animals[size];
+
+        std::cout << std::endl;
+        std::cout << BLUE << "Filling the array with 15 Dogs and 15 Cats:"
+            << RESET << std::endl;
+        for (int i = 0; i < size; i++) {
+            if (i % 2 == 0) {
+                std::cout << YELLOW << i << ": " << RESET;
+                animals[i] = new Dog();
+            } else {
+                std::cout << YELLOW << i << ": " << RESET;
+                animals[i] = new Cat();
+            }
+        }
+
+        std::cout << std::endl;
+        std::cout << BLUE << "Making all the animals speak:" << RESET
+            << std::endl;
+        for (int i = 0; i < size; i++) {
+            std::cout << YELLOW << i << ": " << RESET;
+            animals[i]->makeSound();
+        }
+
+        std::cout << std::endl;
+        std::cout << BLUE << "Deleting the animals:" << RESET << std::endl;
+        for (int i = 0; i < size; i++) {
+            std::cout << YELLOW << i << ": " << RESET;
+            delete animals[i];
+        }
+    }
+    {
+        std::cout << std::endl;
+        std::cout << BLUE << "Creating a Cat using the cat constructor:"
+            << RESET << std::endl;
+        Cat* cat = new Cat();
+
+        std::cout << std::endl;
+        std::cout << BLUE << "Cat's type: " << RESET << std::endl;
+        std::cout << cat->getType() << std::endl;
+        std::cout << BLUE << "Cat's sound: " << RESET << std::endl;
+        cat->makeSound();
+
+        std::cout << std::endl;
+        std::cout << BLUE << "Cat's brain ideas: " << RESET << std::endl;
+        for (int i = 0; i < 101; i++) {
+            std::cout << i << ": " << std::endl;
+            cat->getBrain()->getIdeas(i);
+        }
+
+        std::cout << std::endl;
+        std::cout << BLUE << "Deleting the cat:" << RESET << std::endl;
+        delete cat;
+    }
+    return 0;
+}
